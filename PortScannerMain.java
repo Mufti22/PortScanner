60cc1220cdd6bafcee4398690899a3e44d6e6d09
Malik Fomin <75854478+Mufti22@users.noreply.github.com>
2021-05-04 18:22:52 +0700

    Update PortScannerMain.java

--- PortScannerMain.java
+++ PortScannerMain.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ru.ncedu.tsarev.portscanner;
+
 
 /**
  *
- * @author Алексей
+ * @author Malik Fomin
  */
 public class PortScannerMain {
 
